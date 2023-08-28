dee838d24139d78adc111f26d6dfabaabf407e74
Ameenafz248 <ma6681221@gmail.com>
2023-08-28 18:10:33 +0530

    removed image.js

--- pages/people/index.js
+++ pages/people/index.js
@@ -1,8 +1,34 @@
 import { useState } from "react";
 import styles from '@/styles/People.module.css'
-import { Ameen,Vimal,Vedasree,Sree,Sinadin,Simmon,Rajat,PranavNair,
-    Pranav,Piyush,Neena,Jaseem,Afthab,Madhav,Juby,John,JoelRobin,JoelJohn,Hemanth,Hadif,
-    Cliford,Aabid,Abin,Afraz,Alen,Aravind,Arjun,Bhukya } from "./images.js";
+
+import Aabid from "@/public/images/Team/Aabid Mohamed.jpg"
+import Abin from "@/public/images/Team/Abin_Latheef.jpg"
+import Afraz from "@/public/images/Team/Afraz Ahmed.jpeg"
+import Alen from "@/public/images/Team/Alen_Antony.jpg"
+import Aravind from "@/public/images/Team/Aravind_I_M.png"
+import Arjun from "@/public/images/Team/Arjun_M_Murali.jpeg"
+import Bhukya from "@/public/images/Team/Bhukya Chetan Ram.jpg"
+import Cliford from "@/public/images/Team/Cliford.jpg"
+import Hadif from "@/public/images/Team/Hadif_Yassin_Hameed.JPG"
+import Hemanth from "@/public/images/Team/Hemanth Chodisetti.jpg"
+import JoelJohn from "@/public/images/Team/Joel George John.jpg"
+import JoelRobin from "@/public/images/Team/Joel Robin P.jpg"
+import John from "@/public/images/Team/John_D.png"
+import Juby from "@/public/images/Team/Juby Johnson.JPG"
+import Madhav from "@/public/images/Team/Madhav_Prabhu.jpg"
+import Afthab from "@/public/images/Team/Mohamed_Afthab.jpg"
+import Jaseem from "@/public/images/Team/Muhammed Jaseem.jpg"
+import Neena from "@/public/images/Team/Neena_MS.jpg"
+import Piyush from "@/public/images/Team/Piyush Soni.jpg"
+import Pranav from "@/public/images/Team/Pranav Prashant.jpeg"
+import PranavNair from "@/public/images/Team/Pranav R Nair.jpg"
+import Rajat from "@/public/images/Team/Rajat.jpg"
+import Simmon from "@/public/images/Team/Simon Shaji.jpeg"
+import Sinadin from "@/public/images/Team/Sinadin Shan.jpg"
+import Sree from "@/public/images/Team/Sreevalsan.jpeg"
+import Vedasree from "@/public/images/Team/Vedasree Rodi.jpeg"
+import Vimal from "@/public/images/Team/Vimal Joseph.JPG"
+import Ameen from "@/public/images/Team/Mohammed Ameen.jpg"
 
 import Image from 'next/image'
 
